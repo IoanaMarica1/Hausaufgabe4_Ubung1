a234a3bbc9e4a57550abb7ace13d174e4d1b903b
user <ioanamarica66@gmail.com>
2025-10-19 16:56:52 +0300

    Second and third method updated in class and verified in main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 public class Main{
     public static void main(String[] args) {
         ManageGrades mg = new ManageGrades();
-        int[] note = {22,87,98,30,24,57,39};
+        int[] note = {29, 37, 38, 41, 84, 67};
+        //1
         int[] nichtAusreichender = mg.nichtAusreichenderNote(note);
+        System.out.print("Nicht Ausreichender Note: ");
         for(int n:nichtAusreichender){
-            System.out.print(n + ",");
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        //2
+        double Durchschnittswert = mg.Durchschnittswert(note);
+        System.out.println("Durchschnittswert: " + Durchschnittswert);
+        //3
+        int[] abgerundetenNoten = mg.abgerundetenNoten(note);
+        System.out.print("Abgerundeten Noten: ");
+        for(int n:abgerundetenNoten){
+            System.out.print(n + " ");
         }
         System.out.println();
     }
+
 }
--- src/ManageGrades.java
+++ src/ManageGrades.java
@@ -16,6 +16,45 @@ public class ManageGrades {
         }
         return nichtAusreichender;
     }
+    //2
+    public double Durchschnittswert(int[] Note){
+        int cnt = 0;
+        int sum = 0;
+        for(int n:Note){
+            cnt++;
+            sum+=n;
 
+        }
+        double Durchschnittswert;
+        Durchschnittswert= (double) sum /cnt;
+        return Durchschnittswert;
+    }
+    //3
+    public int[] abgerundetenNoten(int[] Note){
+        int cnt = 0;
+        for(int n:Note){
+            cnt++;
+        }
+        int[] abgerundetenNoten = new int[cnt];
+        int index=0;
+        for(int n:Note){
+            if(n<38){
+                abgerundetenNoten[index]=n;
+                index++;
+            }
+            else{
+                int multiple = (n/5+1)*5;
+                if(multiple-n<3){
+                    abgerundetenNoten[index]=multiple;
+                    index++;
+                }
+                else{
+                    abgerundetenNoten[index]=n;
+                    index++;
+                }
+            }
+        }
+        return abgerundetenNoten;
+    }
 
 }
